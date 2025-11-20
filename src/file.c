5c7a026e86a9e2da177a4491a9cb37eada2dd77b
PoeticDeath <tonylovesdarkred@gmail.com>
2025-11-20 09:26:52 -0500

    Working on mmap, something funny is happening with a cache somewhere.

--- src/file.c
+++ src/file.c
@@ -11,6 +11,166 @@
 #include "Dict.h"
 #include "cspacefs.h"
 
+static int kxcspacefs_getfrag_block(struct inode* inode, sector_t fragment, struct buffer_head* bh_result, int create)
+{
+    struct super_block* sb = inode->i_sb;
+    KMCSpaceFS* KMCSFS = KXCSPACEFS_SB(sb);
+    UNICODE_STRING* fn = inode->i_private;
+    unsigned long long phys = 0;
+    fragment *= 512;
+
+    down_read(KMCSFS->op_lock);
+    unsigned long long index = get_filename_index(*fn, KMCSFS);
+    unsigned long long maxsize = get_file_size(index, *KMCSFS);
+    maxsize -= maxsize % KMCSFS->sectorsize;
+    unsigned long long loc = 0;
+	if (index)
+	{
+		for (unsigned long long i = 0; i < KMCSFS->tablestrlen; i++)
+		{
+			if (KMCSFS->tablestr[i] == *".")
+			{
+				loc++;
+				if (loc == index)
+				{
+					loc = i + 1;
+					break;
+				}
+			}
+		}
+	}
+
+	bool notzero = false;
+	bool multisector = false;
+	unsigned cur = 0;
+	unsigned long long int0 = 0;
+	unsigned long long int1 = 0;
+	unsigned long long int2 = 0;
+	unsigned long long int3 = 0;
+	unsigned long long filesize = 0;
+
+	for (unsigned long long i = loc; i < KMCSFS->tablestrlen; i++)
+	{
+		if (KMCSFS->tablestr[i] == *"," || KMCSFS->tablestr[i] == *".")
+		{
+			if (notzero)
+			{
+				if (multisector)
+				{
+					for (unsigned long long o = 0; o < int0 - int3; o++)
+					{
+                        filesize += KMCSFS->sectorsize;
+                        if (filesize > fragment)
+                        {
+                            phys = KMCSFS->size - KMCSFS->sectorsize - (int3 + o) * KMCSFS->sectorsize;
+                            filesize += KMCSFS->sectorsize;
+                            break;
+                        }
+					}
+                    if (filesize > fragment)
+                    {
+                        break;
+                    }
+				}
+				switch (cur)
+				{
+				case 0:
+                    filesize += KMCSFS->sectorsize;
+                    break;
+				case 1:
+					break;
+				case 2:
+					filesize += int2 - int1;
+					break;
+				}
+                if (filesize > fragment)
+                {
+                    phys = KMCSFS->size - KMCSFS->sectorsize - int0 * KMCSFS->sectorsize;
+                    break;
+                }
+			}
+			cur = 0;
+			int0 = 0;
+			int1 = 0;
+			int2 = 0;
+			int3 = 0;
+			multisector = false;
+			if (KMCSFS->tablestr[i] == *".")
+			{
+				break;
+			}
+		}
+		else if (KMCSFS->tablestr[i] == *";")
+		{
+			cur++;
+		}
+		else if (KMCSFS->tablestr[i] == *"-")
+		{
+			int3 = int0;
+			multisector = true;
+			cur = 0;
+			int0 = 0;
+			int1 = 0;
+			int2 = 0;
+		}
+		else
+		{
+			notzero = true;
+			switch (cur)
+			{
+			case 0:
+				int0 += toint(KMCSFS->tablestr[i] & 0xff);
+				if (KMCSFS->tablestr[i + 1] != *";" && KMCSFS->tablestr[i + 1] != *"," && KMCSFS->tablestr[i + 1] != *"." && KMCSFS->tablestr[i + 1] != *"-")
+				{
+					int0 *= 10;
+				}
+				break;
+			case 1:
+				int1 += toint(KMCSFS->tablestr[i] & 0xff);
+				if (KMCSFS->tablestr[i + 1] != *";" && KMCSFS->tablestr[i + 1] != *"," && KMCSFS->tablestr[i + 1] != *"." && KMCSFS->tablestr[i + 1] != *"-")
+				{
+					int1 *= 10;
+				}
+				break;
+			case 2:
+				int2 += toint(KMCSFS->tablestr[i] & 0xff);
+				if (KMCSFS->tablestr[i + 1] != *";" && KMCSFS->tablestr[i + 1] != *"," && KMCSFS->tablestr[i + 1] != *"." && KMCSFS->tablestr[i + 1] != *"-")
+				{
+					int2 *= 10;
+				}
+				break;
+			}
+		}
+	}
+    up_read(KMCSFS->op_lock);
+
+    if (phys)
+    {
+        map_bh(bh_result, sb, phys / 512 + fragment % KMCSFS->sectorsize / 512);
+    }
+    return 0;
+}
+
+static int kxcspacefs_read_folio(struct file* file, struct folio* folio)
+{
+	return block_read_full_folio(folio, kxcspacefs_getfrag_block);
+}
+
+static int kxcspacefs_writepages(struct address_space* mapping, struct writeback_control* wbc)
+{
+	return mpage_writepages(mapping, wbc, kxcspacefs_getfrag_block);
+}
+
+static int kxcspacefs_write_begin(const struct kiocb* kiocb, struct address_space* mapping, loff_t pos, unsigned len, struct folio** foliop, void** fsdata)
+{
+	return block_write_begin(mapping, pos, len, foliop, kxcspacefs_getfrag_block);
+}
+
+static sector_t kxcspacefs_bmap(struct address_space* mapping, sector_t block)
+{
+	return generic_block_bmap(mapping,block, kxcspacefs_getfrag_block);
+}
+
 /*
  * Called when a file is opened in the kxcspacefs.
  * It checks the flags associated with the file opening mode (O_WRONLY, O_RDWR,
@@ -116,14 +276,29 @@ ssize_t kxcspacefs_write(struct file* file, const char __user* buf, size_t len, 
     return bytes_write;
 }
 
-const struct address_space_operations kxcspacefs_aops = {};
+const struct address_space_operations kxcspacefs_aops =
+{
+    .dirty_folio = block_dirty_folio,
+	.invalidate_folio = block_invalidate_folio,
+	.read_folio = kxcspacefs_read_folio,
+	.writepages = kxcspacefs_writepages,
+	.write_begin = kxcspacefs_write_begin,
+	.write_end = generic_write_end,
+	.migrate_folio = buffer_migrate_folio,
+    .bmap = kxcspacefs_bmap,
+};
 
 const struct file_operations kxcspacefs_file_ops =
 {
     .owner = THIS_MODULE,
+    .read_iter = generic_file_read_iter,
+    .write_iter = generic_file_write_iter,
+    .mmap_prepare = generic_file_mmap_prepare,
     .open = kxcspacefs_open,
     .read = kxcspacefs_read,
     .write = kxcspacefs_write,
     .llseek = generic_file_llseek,
     .fsync = generic_file_fsync,
+    .splice_read = filemap_splice_read,
+    .splice_write = iter_file_splice_write,
 };
